8ff5eb93a4822d7d3cc185ea7e9e1429bab777ff
syf <2654525303@qq.com>
2023-07-28 11:45:24 +0800

    test transparent material

--- src/annotate-tool/Hand.tsx
+++ src/annotate-tool/Hand.tsx
@@ -1,8 +1,17 @@
 import { useGLTF } from "@react-three/drei";
 import { Bone, Mesh, MeshStandardMaterial, SkinnedMesh } from "three";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { useControls } from "leva";
+
+import { LevaColor } from "./misc.ts";
 
 const HandModelPath = '/hand-rigged.glb'
+const ModelColor: LevaColor = {
+    r: 255,
+    g: 0,
+    b: 0,
+    a: 0.5
+}
 
 type HandGLTF = GLTF & {
     nodes: {
@@ -17,7 +26,11 @@ type HandGLTF = GLTF & {
 const Hand = () => {
     const { nodes, materials } = useGLTF(HandModelPath) as unknown as HandGLTF
     const mat = materials.Material;
-    mat.opacity = 0.1
+    mat.transparent = true
+
+    const {c} = useControls({c: ModelColor})
+    mat.color.set(c.r, c.g, c.b)
+    mat.opacity = c.a!
 
     const gltfObj = useGLTF(HandModelPath)
     console.log(gltfObj);
