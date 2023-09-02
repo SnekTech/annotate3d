0cbc055a0047afd0f1551bfab731538d668cfd36
syf <2654525303@qq.com>
2023-09-02 16:28:47 +0800

    reference image

--- src/annotate-tool/AnnotateScene.tsx
+++ src/annotate-tool/AnnotateScene.tsx
@@ -1,7 +1,21 @@
 import { Canvas } from "@react-three/fiber";
-import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
+import { GizmoHelper, GizmoViewport, OrbitControls, useTexture } from "@react-three/drei";
 import { css } from "@emotion/react";
+import { useControls } from "leva"
+
 import { Xbot } from "./Xbot";
+
+function Image() {
+    const texture = useTexture('test-assets/frames/image-005.png')
+    const {imageScale} = useControls({imageScale: {value: 50, min: 10, max: 100}})
+
+    return (
+        <mesh scale={imageScale} position-z={-100}>
+            <planeGeometry attach="geometry"/>
+            <meshBasicMaterial map={texture}/>
+        </mesh>
+    )
+}
 
 const AnnotateScene = () => {
 
@@ -9,16 +23,18 @@ const AnnotateScene = () => {
         backgroundColor: 'lightgray',
     })
 
-    return <Canvas css={canvasStyle} >
-        <OrbitControls makeDefault />
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} />
+    return <Canvas css={canvasStyle}>
+        <OrbitControls makeDefault/>
+        <ambientLight intensity={0.1}/>
+        <directionalLight position={[0, 0, 5]}/>
 
-        <Xbot />
+        <Image/>
+
+        <Xbot/>
 
         {/*{showPerformance ?? <Perf position='bottom-left' />}*/}
         <GizmoHelper>
-            <GizmoViewport />
+            <GizmoViewport/>
         </GizmoHelper>
     </Canvas>
 }
