a6d95c01a0c678ed2def11d9c1e434616a4839dc
syf <2654525303@qq.com>
2023-09-02 15:16:30 +0800

    remove HTML annotation

--- src/annotate-tool/AnnotateScene.tsx
+++ src/annotate-tool/AnnotateScene.tsx
@@ -1,26 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-import { GizmoHelper, GizmoViewport, OrbitControls, Html } from "@react-three/drei";
-import { useControls } from "leva";
+import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
 import { css } from "@emotion/react";
 import { Xbot } from "./Xbot";
 
-function Annotation({ children, ...props }: any) {
-    return (
-        <Html
-            {...props}
-            transform
-            occlude="blending"
-        >
-            <div className="annotation">{children}</div>
-        </Html>
-    )
-}
-
 const AnnotateScene = () => {
-
-    const { pos } = useControls({
-        pos: { x: 0, y: 0, z: 0 }
-    })
 
     const canvasStyle = css({
         backgroundColor: 'lightgray',
@@ -33,14 +16,6 @@ const AnnotateScene = () => {
 
         <Xbot />
 
-        <Annotation position={[pos.x, pos.y, pos.z]}>left_hand</Annotation>
-
-        {/* <mesh ref={box}>
-            <boxGeometry />
-            <meshStandardMaterial color={"blue"} opacity={0.5} transparent/>
-        </mesh>
-
-        <RotateControl type="transform" target={box} /> */}
         {/*{showPerformance ?? <Perf position='bottom-left' />}*/}
         <GizmoHelper>
             <GizmoViewport />
