3df01062f25609d366a7cfd06db70aeb19c2a587
syf <2654525303@qq.com>
2023-12-18 16:25:09 +0800

    load models into scene using drei

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,22 +1,61 @@
 import { Container } from "@mui/material";
-import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { Suspense, useEffect } from 'react'
+import { SkinnedMesh, Object3D, Bone, MeshStandardMaterial } from "three";
+import { GLTF } from "three/examples/jsm/Addons.js";
 
-export function Playground() {
+
+// const gl = useGLTF('/hand-rigged.glb')
+useGLTF.preload('/hand-rigged.glb')
+
+type GLTFNodes = {
+    [nodeName: string]: SkinnedMesh | Object3D | Bone
+    hand: SkinnedMesh
+    Armature_BaseBone: Bone
+}
+
+type HandGLTF = GLTF & {
+    nodes: GLTFNodes
+}
+
+function Hand() {
+    const gltfData = useGLTF('/hand-rigged.glb') as unknown as HandGLTF
+    console.log(gltfData);
     
+    const handGeometry = gltfData.nodes.hand.geometry
+    const bone = gltfData.nodes.Armature_BaseBone
 
     return (
         <>
-            <Container maxWidth="sm" >
-                <Canvas>
+        <mesh 
+            geometry={handGeometry}
+            scale={[0.321, 1, 1]}
+        >
+            <meshStandardMaterial />
+            <primitive object={bone} />
+        </mesh>
+        </>
+    )
+}
+
+export function Playground() {
+
+
+
+    return (
+        <>
+            <Container maxWidth="md" sx={{height: 400}}>
+                <Canvas css={{backgroundColor: 'grey'}}>
                     <ambientLight intensity={0.1} />
                     <directionalLight color={"red"} position={[0, 0, 5]} />
-                    <mesh>
-                        <boxGeometry />
-                        <meshStandardMaterial />
-                    </mesh>
+
+                    <OrbitControls makeDefault />
+                    <Suspense fallback={'loading'}>
+                        <Hand />
+                    </Suspense>
                 </Canvas>
             </Container>
         </>
     )
 }
-
