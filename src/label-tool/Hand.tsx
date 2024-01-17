56d71a91c565ba0331f6918c8d2dabc7375dec82
syf <2654525303@qq.com>
2024-01-17 15:41:06 +0800

    remove unused getBones imperative handle

--- src/label-tool/Hand.tsx
+++ src/label-tool/Hand.tsx
@@ -1,29 +1,25 @@
 import { useHelper } from "@react-three/drei";
-import { Bone, SkeletonHelper, SkinnedMesh } from "three";
+import { SkeletonHelper, SkinnedMesh } from "three";
 import { ForwardedRef, forwardRef, Suspense, useImperativeHandle, useRef } from "react";
-import { getPose, updatePose, useHandModel, Pose } from "./ModelUtils.ts";
+import { getPose, Pose, updatePose, useHandModel } from "./ModelUtils.ts";
 
 
 export type HandModelMethods = {
-    getBones(): Bone[]
     getPose(): Pose
     updatePose(newPose: Pose): void
 }
 
 
-
 function Hand(_props: unknown, ref: ForwardedRef<HandModelMethods>) {
 
     const {skeleton, geometry, bones} = useHandModel()
+    const rootBone = bones[0]
 
     const handMeshRef = useRef<SkinnedMesh>(null!)
     useHelper(handMeshRef, SkeletonHelper)
 
     useImperativeHandle(ref, () => {
         return {
-            getBones(): Bone[] {
-                return bones
-            },
             getPose(): Pose {
                 console.log('getting pose')
                 return getPose(handMeshRef.current)
@@ -44,7 +40,7 @@ function Hand(_props: unknown, ref: ForwardedRef<HandModelMethods>) {
                     skeleton={skeleton}
                     // scale={[0.321, 1, 1]}
                 >
-                    <primitive object={bones[0]}/>
+                    <primitive object={rootBone}/>
                     <meshStandardMaterial color={'hotpink'}/>
                 </skinnedMesh>
 
