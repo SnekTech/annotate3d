ae99534f2f0b8e6d5aefe84c808f2a5e60a4ed88
syf <2654525303@qq.com>
2024-01-29 02:18:27 +0800

    test model file upload

--- src/annotate-manage/AnnotateProject.tsx
+++ src/annotate-manage/AnnotateProject.tsx
@@ -1,6 +1,34 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { Center } from "@chakra-ui/react";
+import axios from "axios"
+
 export function AnnotateProject() {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        console.log(acceptedFiles);
+        const formData = new FormData()
+        formData.append('model', acceptedFiles[0])
+        axios.post('http://localhost:3000/annotate-project/model-upload', formData)
+            .then(res => console.log(res))
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
-        'Annotate Project Management'
+        <div>
+            <p>上传模型</p>
+            <Center
+                borderWidth={'medium'}
+                borderStyle={'dotted'} height={100} width={500}
+                {...getRootProps()}>
+                <input {...getInputProps()}/>
+                {
+                    isDragActive ?
+                        <p>Drop the files here ...</p> :
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                }
+            </Center>
+            <p></p>
+        </div>
     )
 }
