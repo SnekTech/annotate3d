2bb236850f916334020abb7621a98474cf027dba
syf <2654525303@qq.com>
2024-01-30 22:58:54 +0800

    create project form

--- src/annotate-manage/AnnotateProject.tsx
+++ src/annotate-manage/AnnotateProject.tsx
@@ -1,34 +1,66 @@
-import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
-import { Center } from "@chakra-ui/react";
-import axios from "axios"
+import { Button, Container, FormControl, FormErrorMessage, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { HttpClient } from "../core/httpClient.ts";
+import { useUserState } from "../user/userState.ts";
+
+interface IProjectFormData {
+    projectName: string
+    model: FileList
+}
 
 export function AnnotateProject() {
-    const onDrop = useCallback((acceptedFiles: File[]) => {
-        console.log(acceptedFiles);
-        const formData = new FormData()
-        formData.append('model', acceptedFiles[0])
-        axios.post('http://localhost:3000/annotate-project/model-upload', formData)
-            .then(res => console.log(res))
-    }, [])
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting }
+    } = useForm<IProjectFormData>({
+        defaultValues: {
+            projectName: ""
+        }
+    })
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+    const { currentUserId } = useUserState()
+
+    const onSubmit: SubmitHandler<IProjectFormData> = async (data) => {
+        const response = await HttpClient.post("projects/create", {
+            ...data,
+            model: data.model[0],
+            creatorId: currentUserId
+        }, { headers: { "Content-Type": 'multipart/form-data' } })
+        console.log(response);
+    }
 
     return (
-        <div>
-            <p>上传模型</p>
-            <Center
-                borderWidth={'medium'}
-                borderStyle={'dotted'} height={100} width={500}
-                {...getRootProps()}>
-                <input {...getInputProps()}/>
-                {
-                    isDragActive ?
-                        <p>Drop the files here ...</p> :
-                        <p>Drag 'n' drop some files here, or click to select files</p>
-                }
-            </Center>
-            <p></p>
-        </div>
+        <Container>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack spacing={4}>
+                    <FormControl isInvalid={errors.projectName != undefined}>
+                        <FormLabel>项目名称</FormLabel>
+                        <Input placeholder={'XX 项目'}
+                               {...register('projectName', {
+                                   required: { value: true, message: '必须填写项目名称' }
+                               })}
+                        />
+                        <FormErrorMessage>
+                            {errors.projectName ? errors.projectName.message : ''}
+                        </FormErrorMessage>
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>上传模型</FormLabel>
+                        <Input
+                            {...register('model', {
+                                required: '必须提供模型文件'
+                            })}
+                            type={'file'}
+                            multiple={false}
+                        />
+                    </FormControl>
+                </Stack>
+
+                <Button mt={4} colorScheme={'teal'} isLoading={isSubmitting} type={'submit'}>
+                    提交
+                </Button>
+            </form>
+        </Container>
     )
 }
